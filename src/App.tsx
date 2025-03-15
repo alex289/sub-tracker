11315e4deff830844f16fbacddaf8dd7c6612922
alex289 <mccr4fthd@gmail.com>
2025-03-15 23:07:06 +0100

    feat: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+    </main>
+  );
+}
+
+export default App;
