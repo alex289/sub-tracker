f6f9f63e10dd1255740961bcd7766340a7b9b613
alex289 <mccr4fthd@gmail.com>
2025-03-15 23:18:39 +0100

    chore: Format files

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
   pluginReact.configs.flat.recommended,
-  {rules: {
-    semi: 'warn',
-    eqeqeq: 'warn',
-    curly: 'warn',
-    '@typescript-eslint/no-unused-vars': [
+  {
+    rules: {
+      semi: 'warn',
+      eqeqeq: 'warn',
+      curly: 'warn',
+      '@typescript-eslint/no-unused-vars': [
         'warn',
         { argsIgnorePattern: '^_' },
       ],
-    "react/react-in-jsx-scope": "off",
-  }}
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ];
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
   return (
     <main>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </main>
   );
 }
